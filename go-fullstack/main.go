1555b2252a317df1ff3aa3b3fdbe8a163131bf90
amolasg <amol.asg@gmail.com>
2021-05-09 13:11:28 +0530

    implemented CRUD, persisting the data

--- go-fullstack/main.go
+++ go-fullstack/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -60,20 +61,100 @@ func main() {
 // implementing handler functions
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	db := OpenConnection()
 
+	var book Book
+
+	books = []Book{}
+
+	rows, err := db.Query("select * from books")
+	if err != nil {
+		log.Fatal("error in getting records", err)
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+		if err != nil {
+			log.Fatal("error in scan", err)
+		}
+		books = append(books, book)
+	}
+	json.NewEncoder(w).Encode(books)
+	defer rows.Close()
+	defer db.Close()
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
+	db := OpenConnection()
 
+	var book Book
+
+	params := mux.Vars(r)
+
+	rows := db.QueryRow("select * from books where id=$1", params["id"])
+
+	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+	if err != nil {
+		log.Fatal("error in scan", err)
+	}
+	json.NewEncoder(w).Encode(book)
+	defer db.Close()
 }
 func addBook(w http.ResponseWriter, r *http.Request) {
+	db := OpenConnection()
 
+	var book Book
+
+	var bookID int
+
+	json.NewDecoder(r.Body).Decode(&book)
+
+	err := db.QueryRow("insert into books(title,author,year)values($1,$2,$3) RETURNING id;",
+		book.Title, book.Author, book.Year).Scan(&bookID)
+
+	if err != nil {
+		log.Fatal("error in insert record", err)
+	}
+	json.NewEncoder(w).Encode(bookID)
+	defer db.Close()
 }
 
 func updateBook(w http.ResponseWriter, r *http.Request) {
+	db := OpenConnection()
 
+	var book Book
+
+	json.NewDecoder(r.Body).Decode(&book)
+
+	result, err := db.Exec("update books set title=$1,author=$2,year=$3 where id=$4 RETURNING id",
+		&book.Title, &book.Author, &book.Year, &book.ID)
+
+	if err != nil {
+		log.Fatal("error in updating", err)
+	}
+
+	rowsUpdated, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal("error in rows upadted", err)
+	}
+	json.NewEncoder(w).Encode(rowsUpdated)
+	defer db.Close()
 }
 
 func deleteBook(w http.ResponseWriter, r *http.Request) {
+	db := OpenConnection()
 
+	params := mux.Vars(r)
+
+	result, err := db.Exec("delete from books where id=$1", params["id"])
+	if err != nil {
+		log.Fatal("error in delete record")
+	}
+
+	rowsDeleted, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal("error in RowsAffected")
+	}
+	json.NewEncoder(w).Encode(rowsDeleted)
+	defer db.Close()
 }
