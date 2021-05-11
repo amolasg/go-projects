b8f988c8849b48adc98b2c88b2f8d97b17c9e1e4
amolasg <amol.asg@gmail.com>
2021-05-11 10:27:26 +0530

    fix cors error for all delete

--- go-fullstack/main.go
+++ go-fullstack/main.go
@@ -34,5 +34,5 @@ func main() {
 
 	fmt.Println("Server listning on port 8080")
 	// handle cors
-	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(router)))
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS", "DELETE"}), handlers.AllowedOrigins([]string{"*"}))(router)))
 }
